12fdcc2499daf554f8a58ea961b570083693541e
AutumnQYN <qiuyn@bu.edu>
2016-11-03 04:33:12 +0800

    Changed Latitude
    I upload a new Python file and a new CSV file after I changed Latitude from (0,360) to (-180,180)

--- test33_change_csv.py
+++ test33_change_csv.py
@@ -10,30 +10,42 @@ with open('MOD11C1_D_LSTDA_2016-10-14_rgb_360x180 copy.csv', 'r') as inp, open('
 #with open('first_edit.csv', 'w') as out:
 #    inp = csv.reader(open('MOD11C1_D_LSTDA_2016-10-14_rgb_360x180 copy.csv'))
     
-    list1 = []
-    list2 = []
-    list3 = []
+    list1 = ['Latitude']
+    list2 = ['Altitude']
+    list3 = ['Temperature']
+#    建立三个list
     
     writer = csv.writer(out)
     j = 0
+#    j是经度
+    
     for row in csv.reader(inp):
         for i in range(360):
+#            i是纬度
             if row[i] == '99999.0':
-#                list1.append(i)
-#                list2.append(j)
-#                list3.append(row[i])
                 i += 1
+                
             elif row[i] == '-25.0':
                 i += 1
+                
             else:
-                list1.append(i)
-                list2.append(j)
-                list3.append(row[i])
-                i += 1
+                if i <= 180:
+                    list1.append(i)
+                    list2.append(j)
+                    list3.append(row[i])
+                    i += 1
+                else:
+                    list1.append(i-360)
+                    list2.append(j)
+                    list3.append(row[i])
+                    i += 1
+                
         j += 1
         
-    y = zip(list1,list2,list3)
+    y = zip(list1,list3,list2)
     x = list(y)
+#    list4 = [['Latitude','Altitude','Temperature']]
+#    list4.append(x)
 #    print(x)
     
     with open("output.csv", "w") as f:
