37c2d81bafd0bb9bb47904af2f11d0ff6c3fbe64
AutumnQYN <qiuyn@bu.edu>
2016-11-29 05:59:58 +0800

    128 days data of (37,41)
    I collected temperature, activefire, watervapor, and solarinsolation datas of location (37,41) during 128 days.
    Days are from 1st January to 31st January, 8th April to 7th May, 1st July to 30th July, and 1st September to 15th October.

--- try_to_grab_temperature_using_glob.py
+++ try_to_grab_temperature_using_glob.py
@@ -16,21 +16,21 @@ for file_name in Trials1:
     with open(file_name, 'r') as inp:
         list1 = []
         for row in csv.reader(inp):
-            list1.append(row[202])
+            list1.append(row[217])
         
-        lista.append(list1[98])
+        lista.append(list1[49])
 
 valid_temperature = 0
 number_of_average = 0
 
-for k in range(1,96):
+for k in range(1,128):
     if (lista[k] != '99999.0' and lista[k] != '45.0'):
         valid_temperature +=  float(lista[k])
         number_of_average += 1
 
 average_temperature = valid_temperature/number_of_average
 
-for r in range(1,96):
+for r in range(1,128):
     if lista[r] == '99999.0':
         lista[r] = average_temperature
     elif lista[r] == '45.0':
@@ -54,17 +54,19 @@ for file_name in Trials2:
     with open(file_name, 'r') as inp:
         list1 = []
         for row in csv.reader(inp):
-            list1.append(row[202])
+            list1.append(row[217])
         
-        listb.append(list1[98])
+        listb.append(list1[49])
 
-#for i in range(1,96):
-#    if listb[i] == '99999.0':
-#        listb[i] = 0
-#    elif listb[i] == '0.1':
-#        listb[i] = 0
-#    else:
-#        listb[i] = 1
+for i in range(1,96):
+    if listb[i] == '99999.0':
+        listb[i] = 0
+    elif listb[i] == '0.1':
+        listb[i] = 0
+    elif listb[i] == '0':
+        listb[i] = 0
+    else:
+        listb[i] = 1
 
 Trials3 = glob.glob('watervapor*global.csv')
 
@@ -75,25 +77,45 @@ for file_name in Trials3:
     with open(file_name, 'r') as inp:
         list1 = []
         for row in csv.reader(inp):
-            list1.append(row[202])
+            list1.append(row[217])
         
-        listc.append(list1[98])
+        listc.append(list1[49])
         
 valid_watervapor = 0
 number_of_average1 = 0
 
-for k in range(1,96):
+for k in range(1,128):
     if (float(listc[k]) < 10 and listc[k] != '0'):
         valid_watervapor +=  float(listc[k])
         number_of_average1 += 1
 
 average_watervapor = valid_watervapor/number_of_average1
 
-for r in range(1,96):
+for r in range(1,128):
     if float(listc[r]) >= 10:
         listc[r] = average_watervapor
     elif listc[r] == '0':
-        listc[r] = average_watervapor       
+        listc[r] = average_watervapor   
+        
+        
+Trials4 = glob.glob('solarinsolation*global_corrected.csv')
+
+listd = ['Solarinsolation']
+
+k = 0
+
+for file_name in Trials4:
+    with open(file_name, 'r') as inp:
+        list1 = []
+        for row in csv.reader(inp):
+            k += 1
+            list1.append(row[217])
+        
+        listd.append(list1[49])
+        
+
+
+ 
 #p = 0 
 #for j in list6:
 #    j = j-p
@@ -102,10 +124,10 @@ for r in range(1,96):
 #    listc.remove(listc[j])
 #    p += 1
 #        
-y = zip(lista,listb,listc)
+y = zip(lista,listb,listc,listd)
 x = list(y)
 
 #print(x)
-with open("temperature&fire&watervapor_01_07-15_10_22_-8.csv", "w") as f:
+with open("temperature&fire&watervapor&solarinsolation_01_07-15_10_37_41.csv", "w") as f:
         writer = csv.writer(f)
         writer.writerows(x)
